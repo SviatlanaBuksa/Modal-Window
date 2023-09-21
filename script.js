4205d1a1227bf57bb017e4d7fc78867587b175fe
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-09-21 20:49:03 +0200

    Show/close modal functionality added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let openModalBtn = document.querySelector("#open-modal");
+let closeModalBtn = document.querySelector("#close-modal");
+let modalWindow = document.querySelector(".modal-overlay");
 
+openModalBtn.addEventListener(
+  "click",
+  () => (modalWindow.style.visibility = "visible")
+);
+
+closeModalBtn.addEventListener(
+  "click",
+  () => (modalWindow.style.visibility = "hidden")
+);
