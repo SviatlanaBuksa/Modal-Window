143b43cfeec9fcb8ca53fe7e980e9487c0f8a437
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-09-21 21:04:10 +0200

    Made visibality in separate css class

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ let openModalBtn = document.querySelector("#open-modal");
 let closeModalBtn = document.querySelector("#close-modal");
 let modalWindow = document.querySelector(".modal-overlay");
 
-openModalBtn.addEventListener(
-  "click",
-  () => (modalWindow.style.visibility = "visible")
+openModalBtn.addEventListener("click", () =>
+  modalWindow.classList.add("show-modal")
 );
 
-closeModalBtn.addEventListener(
-  "click",
-  () => (modalWindow.style.visibility = "hidden")
+closeModalBtn.addEventListener("click", () =>
+  modalWindow.classList.remove("show-modal")
 );
